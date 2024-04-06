888916f2ae2d72f435cf4a8a902304c17d0668c1
pathania0007 <160423986+pathania0007@users.noreply.github.com>
2024-04-06 03:38:32 -0400

    initial commit

---
+++ src/main/java/com/sanjay/bhaisanjay/BhaisanjayApplication.java
@@ -0,0 +1,13 @@
+package com.sanjay.bhaisanjay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhaisanjayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BhaisanjayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanjay/bhaisanjay/BhaisanjayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanjay.bhaisanjay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BhaisanjayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
